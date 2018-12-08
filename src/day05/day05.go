a8c6ccf26d5706ca29980cbdc8eff6a4d74aeb2d
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2018-12-08 00:00:05 -0500

    Day 5: Finished part 1

--- src/day05/day05.go
+++ src/day05/day05.go
@@ -1,102 +1,72 @@
 package day05
 
 import (
+	"adventOfCode"
 	"fmt"
-	// "strings"
-	// "adventOfCode"
 )
 
-func checkString(str string) string {
-	Cache := make(map[string]string)
-
-	for _, a := range str {
-		Cache[string(a)] = string(a)
-		fmt.Printf("Cache[%s]: %s \n", string(a), Cache[string(a)])
-	}
-
-	for i, a := range str {
-		for _, b := range str[:i] {
-			if a^b == 32 {
-				Cache[string(a)+string(b)] = ""
+func reactPolymer(polymer string) string {
+	var newPolymer []byte
+	for i := range polymer {
+		molecule := polymer[i]
+		if len(newPolymer) == 0 {
+			newPolymer = append(newPolymer, molecule)
+		} else {
+			prevMolecule := newPolymer[len(newPolymer)-1]
+			if prevMolecule^molecule != 32 {
+				newPolymer = append(newPolymer, molecule)
 			} else {
-				Cache[string(a)+string(b)] = string(a) + string(b)
+				newPolymer = newPolymer[0 : len(newPolymer)-1]
 			}
-			fmt.Printf("Cache[%s]: %s \n", string(a)+string(b), Cache[string(a)+string(b)])
 		}
 	}
-
-	// var newString string
-	for i := 3; i < len(str); i++ {
-		for j := 0; i+j < len(str); j++ {
-			substring := str[j:(j + i)]
-			for k := 1; k < i; k++ {
-				a := Cache[substring[:k]]
-				b := Cache[substring[k:]]
-				fmt.Println(a, a[len(a)-1], b, Cache[b])
-				// fmt.Println("EE", a, a[len(a)-1]+b[0], b[1:])
-				// cached := a[:len(a)-1] + Cache[string(a[len(a)-1])+string(b[0])] + b[1:]
-				// fmt.Printf("HI: %s:%s | %s:%s\n", a, b, Cache[a], Cache[b])
-				// if _, ok := Cache[substring]; ok {
-				// 	if len(cached) < len(Cache[substring]) {
-				// 		Cache[substring] = cached
-				// 	}
-				// } else {
-				// 	Cache[substring] = cached
-				// }
-
-			}
-			fmt.Printf("Cache[%s]: %s \n", substring, Cache[substring])
-		}
-	}
-
-	// fmt.Println(str, Cache[str])
-
-	return "hi"
+	return string(newPolymer)
 }
 
 func PartOne() {
 
-	polymer := "dabAcCaCBAcCcaDA"
-
-	changed := false
-
-	new_polymer := polymer
-	for i, unit := range new_polymer {
-		var new_unit string
-		if i+1 != len(new_polymer) {
-			if changed {
-				i = i - 1
-			}
-			before := new_polymer[:i]
-			after := new_polymer[i+2:]
-			next_unit := new_polymer[i+1]
-
-			if byte(unit)^next_unit == 32 {
-				new_unit = ""
-			} else {
-				new_unit = string(unit) + string(next_unit)
-			}
-			fmt.Println(i, before+" "+string(unit)+string(next_unit)+" "+after)
-			fmt.Println(i, before+" "+new_unit+" "+after)
-			polymer = new_polymer
-			new_polymer = before + new_unit + after
-			if polymer != new_polymer {
-				changed = true
-			} else {
-				changed = false
-			}
-			fmt.Println(i, polymer)
-			fmt.Println(i, new_polymer)
-		}
-	}
-	fmt.Println(polymer)
-	fmt.Println(new_polymer)
+	polymer := adventOfCode.ReadInputFile("05", "input.txt")[0]
+	newPolymer := reactPolymer(polymer)
+	fmt.Printf("Answer: %d \n", len(newPolymer))
 
 	return
-	// fmt.Printf("Answer: %d\n", 42)
 }
 
 func PartTwo() {
+
+	polymer := adventOfCode.ReadInputFile("05", "input.txt")[0]
+	// polymer = "dabAcCaCBAcCcaDA"
+	numberOfBonds := make(map[string]int)
+	var newPolymer []byte
+	for i := range polymer {
+		molecule := polymer[i]
+		if len(newPolymer) == 0 {
+			newPolymer = append(newPolymer, molecule)
+		} else {
+			prevMolecule := newPolymer[len(newPolymer)-1]
+			if prevMolecule^molecule != 32 {
+				newPolymer = append(newPolymer, molecule)
+			} else {
+				newPolymer = newPolymer[0 : len(newPolymer)-1]
+				if _, ok := numberOfBonds[string(prevMolecule)]; ok {
+					numberOfBonds[string(prevMolecule)] += 2
+					numberOfBonds[string(molecule)] += 2
+
+				} else {
+					numberOfBonds[string(prevMolecule)] = 2
+					numberOfBonds[string(molecule)] = 2
+				}
+			}
+		}
+	}
+	answer := 0
+	for i := range numberOfBonds {
+		if numberOfBonds[i] > answer {
+			answer = numberOfBonds[i]
+		}
+	}
+
+	fmt.Printf("Answer: %d \n", answer)
+
 	return
-	// fmt.Printf("Answer: %d\n", 42)
 }
